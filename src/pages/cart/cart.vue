42789f98a010f4cc3125f076d3e1535ada44926c
lzp-mayun <liuzepeng0200@163.com>
2023-07-18 17:38:07 +0800

    first commit: initialize repository

---
+++ src/pages/cart/cart.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="cart">
+    123344
+    <calendar />
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view>
+    <!-- 插入模式 -->
+    <uni-calendar class="uni-calendar--hook" :showMonth="false" />
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
